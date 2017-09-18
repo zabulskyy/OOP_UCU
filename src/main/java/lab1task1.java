bab7d2acf7daefc0d602e8d406a72da569da9f2c
zabulskyy <zabulskyy@ucu.edu.ua>
2017-09-18 10:59:32 +0300

    Initial commit

---
+++ src/main/java/lab1task1.java
@@ -0,0 +1,13 @@
+public class lab1task1 {
+    public static void main(String[] args) {
+        myMethod(1);
+    }
+
+    static void myMethod(int num) {
+        System.out.println("Hello, " + num);
+    }
+
+    static void myMethod(String name) {
+        System.out.println("Hello, " + name);
+    }
+}
