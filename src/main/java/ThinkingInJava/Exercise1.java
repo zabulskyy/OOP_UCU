7dfcb922d7b7dd24abe15aad27f494d9581dd550
vzabulskyy <v.zabulskyy@gmail.com>
2017-10-06 19:09:08 +0300

    playing with overloading

--- src/main/java/ThinkingInJava/Exercise1.java
+++ src/main/java/ThinkingInJava/Exercise1.java
@@ -4,18 +4,18 @@ package ThinkingInJava;
 /* This simple example shows that primitives have default initialization
 * Objects are null by default */
 
+
 public class Exercise1 {
 
-    static String str;
-    static int integer;
-    static char character;
-    static Integer integerClass;
+    int f(){
+        return 1;
+    }
+
+    int f(int x){
+        return x;
+    }
 
     public static void main(String[] args) {
 
-        System.out.println(integer);
-        System.out.println(str);
-        System.out.println(character);
-        System.out.println(integerClass);
     }
 }
